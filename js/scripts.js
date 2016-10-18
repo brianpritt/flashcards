9d788c4fd9e39beff1b70c73fdc65d81a1a7a549
Brad Copenhaver and Brian Pritt <brad@>
2016-10-18 12:00:12 -0700

    create initial files for flashcard page, add layout filler and jquery interaction

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  $("#card1 .panel-heading").click(function(){
+    $("#card1 .panel-body").toggle();
+    $("#card1 .panel-heading").toggle();
+  });
+  $("#card1 .panel-body").click(function(){
+    $("#card1 .panel-heading").toggle();
+    $("#card1 .panel-body").toggle();
+  });
+  $("#card2 .panel-heading").click(function(){
+    $("#card2 .panel-body").toggle();
+    $("#card2 .panel-heading").toggle();
+  });
+  $("#card2 .panel-body").click(function(){
+    $("#card2 .panel-heading").toggle();
+    $("#card2 .panel-body").toggle();
+  });
+  $("#card3 .panel-heading").click(function(){
+    $("#card3 .panel-body").toggle();
+    $("#card3 .panel-heading").toggle();
+  });
+  $("#card3 .panel-body").click(function(){
+    $("#card3 .panel-heading").toggle();
+    $("#card3 .panel-body").toggle();
+  });
+  $("#card4 .panel-heading").click(function(){
+    $("#card4 .panel-body").toggle();
+    $("#card4 .panel-heading").toggle();
+  });
+  $("#card4 .panel-body").click(function(){
+    $("#card4 .panel-heading").toggle();
+    $("#card4 .panel-body").toggle();
+  });
+  $("#card5 .panel-heading").click(function(){
+    $("#card5 .panel-body").toggle();
+    $("#card5 .panel-heading").toggle();
+  });
+  $("#card5 .panel-body").click(function(){
+    $("#card5 .panel-heading").toggle();
+    $("#card5 .panel-body").toggle();
+  });
+  $("#card6 .panel-heading").click(function(){
+    $("#card6 .panel-body").toggle();
+    $("#card6 .panel-heading").toggle();
+  });
+  $("#card6 .panel-body").click(function(){
+    $("#card6 .panel-heading").toggle();
+    $("#card6 .panel-body").toggle();
+  });
+  $("#card7 .panel-heading").click(function(){
+    $("#card7 .panel-body").toggle();
+    $("#card7 .panel-heading").toggle();
+  });
+  $("#card7 .panel-body").click(function(){
+    $("#card7 .panel-heading").toggle();
+    $("#card7 .panel-body").toggle();
+  });
+  $("#card8 .panel-heading").click(function(){
+    $("#card8 .panel-body").toggle();
+    $("#card8 .panel-heading").toggle();
+  });
+  $("#card8 .panel-body").click(function(){
+    $("#card8 .panel-heading").toggle();
+    $("#card8 .panel-body").toggle();
+  });
+});
